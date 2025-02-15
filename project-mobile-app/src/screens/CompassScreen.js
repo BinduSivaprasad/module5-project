a6a087f72d2b92d95703f3bace7ab3425aeb2841
Mmanyuu <omanyu91@gmail.com>
2025-02-15 16:21:36 +0800

    first commit

---
+++ project-mobile-app/src/screens/CompassScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function CompassScreen() {
+  return (
+    <View>
+      <Text>This is Compass Screen</Text>
+    </View>
+  );
+}
+
+export default CompassScreen;
